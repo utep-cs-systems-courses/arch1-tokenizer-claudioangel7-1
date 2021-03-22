18dff93433f9cbc870267a643f972fbb7fdbd651
claudioangel7 <cagarcia38@miners.utep.edu>
2021-03-22 13:40:34 -0600

    added history.c file and methods, and add history functionality to tokenizeUI.c file

--- src/history.c
+++ src/history.c
@@ -1,27 +1,83 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "tokenizer.h"
 #include "history.h"
 
+int counter;
 
 /* Initialize the linked list to keep the history. */
-//List* init_history(){}
+List* init_history()
+{
+  List *temp;
+  temp = (*List)malloc(sizeof(List));
+  return temp;
+}
 
 /* Add a history item to the end of the list.
    List* list - the linked list
    char* str - the string to store
 */
 
-//void add_history(List *list, char *str){}
+
+void add_history(List *list, char *str)
+{
+    counter++;
+    
+    if(counter == 1){
+        Item *item_add = (*Item)malloc(sizeof(Item));
+        item_add->id = counter;
+        item_add->str;
+        list->root = item_add;
+    }else{
+        Item *node = list->root;
+        while(node->next != NULL){
+            node = node->next;
+        }
+        node->next = (*Item)malloc(sizeof(Item));
+        node->next->str;
+        node->next->next = NULL;
+        node->next->id = counter;
+    }
+    
+}
 
 /* Retrieve the string stored in the node where Item->id == id.
    List* list - the linked list
    int id - the id of the Item to find */
 
-//char *get_history(List *list, int id){}
+
+char *get_history(List *list, int id)
+{
+    Item *node = list->root;
+    while(node != NULL){
+        if(node->id == id){
+            return node->str;
+        }
+        node = node->str;
+    }
+    
+}
 
 // *Print the entire contents of the list. */
-//void print_history(List *list){}
+
+
+void print_history(List *list)
+{
+    Item *node = list-> root;
+    while(node != NULL){
+        printf("%d: \"%s\"\n", node->id, node, node->str);
+        node = node->next;
+    }
+}
 
 // *Free the history list and the strings it references. */
-//void free_history(List *list){}
+
+void free_history(List *list)
+{
+    Item *node;
+    while((node = list->root) != NULL){
+        list->root = list->root->next;
+        free(node);
+    }
+}
 
--- src/tokenizerUI.c
+++ src/tokenizerUI.c
@@ -3,11 +3,13 @@
 #include <ctype.h>
 #include "history.h"
 #include "tokenizer.h"
-#include "tokenizer.c"
+
 
 int main()
 {
-
+    
+    //List *history = init_history();
+    int id[10];
     char str[100];
     char test[50];
     char line[50];
@@ -18,7 +20,7 @@ int main()
     while(ans != 0)
     {
         printf("\nTest menu\n");
-        printf("1.space_char \n2.non_space_char \n3.word_start \n4.word_end \n5.count_words \n6.Tokenize \n7.exit \n8.TOKENIZER \n ");
+        printf("1.space_char \n2.non_space_char \n3.word_start \n4.word_end \n5.count_words \n6.TOKENIZER \n7.print history");
         
         fgets (line, sizeof(line), stdin);
         r = sscanf (line, "%d", &ans);
@@ -31,42 +33,35 @@ int main()
         {
             case 1:
                 printf("Testing space_char, input a word\n");
-                fgets(test, sizeof(test), stdin);
+                fgets(test, strlen(test), stdin);
                 printf("\n%d\n", space_char(test[0]));
                 break;
                 
             case 2:
                 printf("Testing non_space_char, input a word\n");
-                fgets(test, sizeof(test), stdin);
+                fgets(test, strlen(test), stdin);
                 printf("\n%d\n", non_space_char(test[0]));
                 break;
                 
             case 3:
                 printf("Testing word_start, input a word\n");
-                fgets(test, sizeof(test), stdin);
+                fgets(test, strlen(test), stdin);
                 printf("\n%s\n", word_start(test));
                 break;
                 
             case 4:
                 printf("Testing word_end, input a word\n");
-                fgets(test, sizeof(test), stdin);
+                fgets(test, strlen(test), stdin);
                 printf("\n%s\n", word_terminator(test));
                 break;
                 
             case 5:
                 printf("Testing count_words, input a phrase\n");
-                fgets(test, sizeof(test), stdin);
+                fgets(test, strlen(test), stdin);
                 printf("\n%d\n", count_words(test));
                 break;
                 
             case 6:
-                printf("Testing tokenize, input a phrase\n");
-                fgets(test, sizeof(test), stdin);
-                char **s = tokenize(test);
-                print_tokens(s);
-                break;
-                
-            case 8:
                   while(1)
                     {
                     printf("Input a phrase: ");
@@ -78,13 +73,30 @@ int main()
                         str[len-1] = '\0';
                     }
                     
-                    char** tokens = tokenize(str);
+                    //add_history(history, str);
+                    char **tokens = tokenize(str);
                     
                     print_tokens(tokens);
                     }
                 
-            case 7:
+            case 10:
                 ans = 0;
+            /*
+            case 7:
+                print_history(history);
+                
+            case 8:
+                char retrievedStr[50];
+                printf("Input an id to retrieve a string item\n");
+                fgets(id, 10, stdin);
+                retrievedStr = get_history(history, id);
+                char **tokens = tokenize(retrievedStr);
+                print_tokens(tokens);
+                
+            case 9:
+                printf("Free history\n");
+                free_history(history);
+                */
             
             
             default:
