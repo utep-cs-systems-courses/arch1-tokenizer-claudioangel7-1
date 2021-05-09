77c91144ce9c9667195f45571a82d1f44dc98c92
claudioangel7 <cagarcia38@miners.utep.edu>
2021-05-09 16:29:30 -0600

    Changed simpleUI to have the main method and erased tokenizerUI

--- src/simpleUI.c
+++ src/simpleUI.c
@@ -1,14 +1,72 @@
 #include <stdio.h>
-int main(){
- 
-  char ans[50];
-  while(1){
-    printf("Input a word or '0' to exit: ");
-    fgets(ans,50,stdin);
-    
-    if(ans[0] == '0')
+#include <stdlib.h>
+#include "history.h"
+#include "tokenizer.h"
+#define max_lim 100
+
+
+int main()
+{
+  char str[max_lim]; /* string to tokenize */
+
+  List *list = init_history(); /*list to keep history */
+
+  char num;
+
+  printf("Welcome to tokenizer!\n");
+  printf("Here you will be able to tokenize many strings and keep history of them\n");
+  printf("Now read the following instructions to begin\n");
+  printf("\n");
+  
+  while(num != -1){
+
+    printf("Select 1 to tokenize a new string\n");
+    printf("Select 2 to view history\n");
+    printf("Select 0 to quit the program\n");
+
+    scanf("%d", &num);
+
+    char hold; //in order to get new strings
+    scanf("%c", &hold);
+
+    if(num == 1){
+      printf("Enter string\n");
+      fgets(str,60,stdin);
+      char **token = tokenize(str);
+      print_tokens(token);
+      free_tokens(token);
+
+      printf("\n");
+
+      /*getting first word of sentence*/
+      char *first = word_start(str);
+      printf("The first word of the string is: ");
+      printf("%c\n", first);
+
+      /* getting the number of words*/
+      int num_words = count_words(str);
+      printf("The number of words in the string is: ");
+      printf("%d\n", num_words);
+
+      add_history(list, str); //adding the token into the history
+     
+      printf("\n ");
+    }
+    if(num == 0){
       break;
-    printf("%s\n",ans);
+    }
+
+    if(num == 2){
+      printf("Here is the complete history of everything you have tokenized!\n");
+      print_history(list);
+
+    }
   }
-  return (0);
+
+  free_history(list);
+  printf("Thank you for using tokenizer\n");
+  printf("please come back later\n");
+  
+
+  return 0;
 }
